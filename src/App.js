eddc446ffcc4c9adb69752cf7b271e74200c646d
Wwright91 <wisdomwright-martin@pursuit.org>
2022-10-17 15:32:33 -0400

    added search

--- src/App.js
+++ src/App.js
@@ -1,88 +1,55 @@
-// import "./App.css";
-// import { useState } from "react";
-
-// function App() {
-//   const [drinks, setDrinks] = useState([]);
-//   const [letter, setLetter] = useState("");
-//   // const [error, setError] = useState(false)
-
-//   function setInput(e) {
-//     setLetter(e.target.value);
-//   }
-
-//   function getDrinksOnClick() {
-//     fetch(
-//       `https:/www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`
-//     ).then((res) =>
-//       res.json().then((data) => {
-//         console.log(data);
-//         setDrinks(data.drinks);
-//       })
-//     );
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>Welcome To My Lounge</h1>
-//       <br />
-//       <h2>Search Drinks Below</h2>
-//       <br />
-//       <input
-//         type="text"
-//         value={letter}
-//         onChange={setInput}
-//         placeholder="input a letter"
-//       />
-//       <button onClick={getDrinksOnClick}>Get Drinks</button>
-//       <h2 />
-//       Drinks That Begin With The Letter: "{letter.toUpperCase()}"<h2 />
-//       <br />
-//       {drinks.map((d) => {
-//         return d==="null" ? (
-//           <h3>Drink not available</h3>
-//         ) : (
-//           <div key={d.idDrink}>
-//             <h3>{d.strDrink}</h3> <img src={d.strDrinkThumb} />
-//             <br />
-//             <br />
-//             <div>{d.strInstructions}</div>
-//             <br />
-//             <br />
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   const [drinks, setDrinks] = useState([]);
+  const [input, setInput] = useState("");
 
+  function handleInput(e) {
+    setInput(e.target.value);
+  }
   function getDrinksOnClick() {
-
-    fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", {
+    fetch(`https://the-cocktail-db.p.rapidapi.com/search.php?s=${input}`, {
       method: "GET",
       headers: {
         "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
         "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
       },
-    })
-      .then((res) => res.json())
-      .then((data) => {
+    }).then((res) =>
+      res.json().then((data) => {
+        console.log(data);
         setDrinks(data.drinks);
-      });
+      })
+    );
   }
 
   return (
-    <div>
+    <div className="App">
+      <h1>Welcome To My Lounge</h1>
+      <br />
+      <h2>Search Drinks Below</h2>
+      <br />
+      <input
+        type="text"
+        value={input}
+        onChange={handleInput}
+        placeholder="input a drink"
+      />
       <button onClick={getDrinksOnClick}>Get Drinks</button>
+      <h2 />
+      Drinks With: "{input.toUpperCase()}"<h2 />
+      <br />
       {drinks.map((d) => {
-        return <div>{d.strDrink}</div>;
+        return (
+          <div key={d.idDrink}>
+            <h3>{d.strDrink}</h3> <img src={d.strDrinkThumb} />
+            <br />
+            <br />
+            <div>{d.strInstructions}</div>
+            <br />
+            <br />
+          </div>
+        );
       })}
     </div>
   );
